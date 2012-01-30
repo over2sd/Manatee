3fe8fc878b9e58eb22d99147b4cb6d58681169dc
Lincoln Sayger <skrey_pub@cox.net>
2012-01-30 13:30:24 -0600

    Completed transition to SQL and Jinja2.

--- ccowhelp.py
+++ ccowhelp.py
@@ -9,8 +9,9 @@ import cgi
 import cgitb; cgitb.enable()
 import codecs
 from sqlalchemy import (create_engine, Column)
+from jinja2 import (Environment, FileSystemLoader)
 
-BUILDNUM = "064"
+BUILDNUM = "095"
 MYNAME = os.path.basename(sys.argv[0])
 
 import tmpvalues
@@ -20,7 +21,6 @@ def showTop(t):
   """Given a title string, prints the stuff needed at the top of our XHTML
   documents.
   """
-  print "Content-Type: text/html\n\n",
   print "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n<html xmlns=\"http://www.w3c.org/1999/xhtml\" lang=\"EN\">\n<head>\n\t<title>" + t + "</title>\n\t<link rel=\"stylesheet\" type=\"text/css\" href=\"main.css\" />\n</head>\n<body>"
 
 
@@ -76,29 +76,51 @@ def getStage(code):
   if code == "XXXX": loc = 0
   return loc
 
-def glotCrumbs(code,stage,lang):
-  """This function prints a trail of breadcrumbs in the appropriate
-  language.
+def glotCrumbs(sql,code,stage,lang):
+  """This function returns a list loaded with one or more tuples containing
+  the strings needed to print a trail of breadcrumbs in the
+  appropriate language.
   """
-  print "<!-- Code: " + str(code) + " stage " + str(stage) + " language: " + lang + " -->"
+  crumbs = []
   if code[0] == 'x' or stage == 0: # already at the top
-    print "\n\t\t<ul class=\"loaf\" id=\"breadtrail\">\n\t\t\t<li class=\"crumb\" id=\"sm05\"><a href=\"" + MYNAME + "\">",
-    glot("slan",lang,stage)
-    print "</a></li>\n\t\t</ul>"
-    return
+    a = "5"
+    b = MYNAME
+    c = glot("slan",sql,lang)
+    crumbs.append((a,b,c))
+    return crumbs
   else:
-    print "\t\t<ul class=\"loaf\" id=\"breadtrail\">\n\t\t\t<li class=\"crumb\" id=\"sm06\"><a href=\"" + MYNAME + "?lang=" + lang + "&cowc=xxxx\">",
-    glot("soop",lang,stage)
-    print "</a> &gt;</li>"
+    a = "6"
+    b = MYNAME + "?lang=" + lang
+    c = glot("soop",sql,lang,stage)
+    crumbs.append((a,b,c))
+
     ic = 1
     while ic < stage and code[ic] != 'x':
-      print "\t\t\t<li class=\"crumb\" id=\"sm0" + str(ic + 6) + "\"><a href=\"" + MYNAME + "?lang=" + lang + "&cowc=" + str(code[:ic]) + "\">",
+      a = str(ic + 6)
+      b = MYNAME + "?lang=" + lang + "&cowc=" + str(code[:ic])
       sc = code[:ic]
       while len(sc) < 4: sc += 'x'
-      glot(sc,lang,ic)
-      print "</a> &gt;</li>"
+      c = glot(sc,sql,lang,ic)
+      crumbs.append((a,b,c))
       ic = ic + 1
-    print "\n\t\t</ul>"
+    return crumbs
+
+def glotSubs(sql,code,stage,lang):
+  """Given a database connection, a category code, a language
+  abbreviation, and a code stage (int), this function returns
+  a list of tuples containing the code, digit, and description
+  of its 16 subcategories.
+  """
+  subs = []
+  for i in range(16):
+    c = hexchar(i)
+    scode = code[:stage]
+    scode += c.lower()
+    scode = cleanCode(scode)
+    sdesc = glot(scode,sql,lang,stage + 1)
+    subs.append((scode,c,sdesc))
+  return subs
+
 
 def glotInf(code,lang,verbose = 1):
   """Given a category code, a language, and instruction on whether
@@ -132,27 +154,55 @@ def explainCats(lang):
   showSubs("yxxx",lang,3)
 # I think this is incomplete, too
 
-def glot(code,lang,stage = 5):
+def glot(code,sql,lang,stage = 5):
   """Given a category code, a language abbreviation (e.g. "en"), and a
-  stage, this function will print the category's description in that
-  language, if available, and fall back to English, if not.
+  stage, this function returns the category's description in that
+  language, if available, and falls back to English, if not.
   """
+  outstring = ""
+  nodict = False
   # Some day, SQL...
   # for now, a dict:
-  if code in dict:
-    print dict[code],
-  elif stage == 5:
-    if code == "sms1":
-      print "'Missing string' string not found! The sky is falling!"
-      exit(2)
-    glot("sms1",lang)
+  if code[0] == 's' or code[0] == 'y':
+    if lang in dict:
+      if code in dict[lang]:
+        outstring += dict[lang][code]
+      elif stage == 5:
+        if code == "sms1":
+          return "'Missing string' string not found! The sky is falling!"
+        glot("sms1",lang)
+      else:
+        nodict = True
+    else:
+      nodict = True
   else:
-    print "[Unassigned] ",
-    laspects = ["Hol","Ras","Dua","Chi","Ter","Fum","Sek","Zab","Med","Neu","Uay","Arz","Pax","Ord","Iyu","Ech"]
-    aspect = unhexchar(code[stage - 1])
-    print "<!-- Aspect: " + str(aspect) + " stage: " + str(stage) + "-->",
-    if aspect != None:
-      print "(" + laspects[aspect] + ")",
+    nodict = True
+  if nodict:
+#    if stage == 5: stage = 4
+    cmd = "SHOW TABLES LIKE 'categories_" + lang + "';"
+    result = sql.execute(cmd)
+#    print "<!-- Rows: " + str(result.rowcount) + " -->"
+    if result.rowcount:
+      code = code.replace('x','o') # x isn't allowed in the SQL because of 0x combinations
+      code = code.replace('X','o') # Special for no category given
+      cmd = "SELECT ctext FROM categories_" + lang + " WHERE ccode = '" + code + "' LIMIT 1;"
+      result = sql.execute(cmd)
+      if result.rowcount:
+        for row in result:
+          return unsql(row['ctext'])
+      else:
+        outstring = "[Unassigned] (%s)"
+        laspects = ["Hol","Ras","Dua","Chi","Ter","Fum","Sek","Zab","Med","Neu","Uay","Arz","Pax","Ord","Iyu","Ech"]
+        aspect = unhexchar(code[stage - 1])
+ #    print "<!-- Aspect: " + str(aspect) + " stage: " + str(stage) + "-->",
+        if aspect != None:
+          outstring = outstring.replace("%s",laspects[aspect])
+    elif lang != "en":
+      glot(code,sql,"en")
+    else:
+      return "Database query failed: No languages found!"
+    #sql
+  return outstring
 
 def sglot(code,lang,s):
   """Given a category code, a language abbreviation, and a string,
@@ -199,74 +249,104 @@ def showSubs(code,lang,stage):
   print "\n<p class=\"clearit\"> </p>"
 
 
+def unsql(strin):
+  """Given a string, replaces SKR-type SQL-safe escapes with their unsafe counterparts."""
+  strout = ""
+  strout = strin.replace("~9","&")
+  strout = strout.replace("~8","*")
+  strout = strout.replace("~7","#")
+  strout = strout.replace("~6","x")
+  strout = strout.replace("~5","--")
+  strout = strout.replace("~4","=")
+  strout = strout.replace("~3",'"')
+  strout = strout.replace("~2",";")
+  strout = strout.replace("~1","'")
+  strout = strout.replace("~0","~")
+  return strout
+
+
 def main():
   """Run if the program is called from the shell, of course."""
-  conf = open('sql.cfg') # change to: conf = open('manatee.conf')
-  lines = conf.readlines()
-  engine = create_engine("%s/ccow" % lines[0].strip())
+  print "Content-Type: text/html\n\n",
+
+  lines = []
+  config = {}
+  var = {}
+  try:
+    with codecs.open('manatee.conf','rU','utf-8') as conf:
+      lines = conf.readlines()
+  except IOError as e:
+    print " Could not open configuration file: %s" % e
+
+  for line in lines:
+    try:
+      line = line.strip()
+      if line:
+        values = [x.strip() for x in line.split('=')]
+        config[values[0]] = values[1]
+    except Exception as e:
+      print "There was an error in the configuration file: %s" % e
+  # TODO: Any strings from the config file that might be displayed or passed into the SQL server need to be validated here.
+
+  here = os.path.dirname(os.path.abspath(__file__))
+  env = Environment(loader=FileSystemLoader(here))
+  template = env.get_template(os.path.join("template",'ccowhelp.jtl'))
+  #  validateConfig(config)
+
+  # Create connection to SQL server (currently MySQL)
+  engine = create_engine("%(host)s/%(base)s" % {'host':config["host"],'base':config["base"] } )
   sqlconn = engine.connect()
+  # Grab GET data from URL
   query = cgi.FieldStorage()
-  lang = query.getvalue("lang","en")
+  lang = query.getvalue("lang","en")[:2]
+  var['lang'] = lang
   code = query.getvalue("cowc","XXXX")
   code = str(cleanCode(code))
-# check for query string
+  if "site" not in config:
+    config['site'] = "Manatee"
+  # check for query string
   if len(query) == 0:
-# if no query, offer lang selection
-    showTop("Manatee: Select Language")
-    langSel()
+    # if no query, offer lang selection
+    var['title'] = "Select Language"
   else:
-# if query provided, show a page...
-    showTop("Manatee: " + str(code)) # ...with a title grabbed from the database...
-    stage = getStage(code)
+  # if query provided, show a page...
+    var['code'] = code
+    var['title'] = var['code']
+    stage = getStage(var['code'])
+    var['stage'] = stage
     cat00 = str(code[:stage])
-    cattrim = cat00
-    for i in range(4 - stage): cat00 += '0'
-    if code[0] == 'y':
-      glotCrumbs("0xxx",1,lang)
-      if code[3] == 'x':
-        explainCats(lang)
-      else:
-        print "\t\t\t<li class=\"crumb\" id=\"sm07\"><a href=\"" + MYNAME + "?lang=" + lang + "&cowc=yxxx\">",
-        glot("sxpl",lang)
-        print "</a> &gt;</li>"
-        print "\n\t\t</ul>"
-        print "\n\t\t<h1 id=\"sm11\">#*" + code[-1:] + ": ",
-        glot(code,lang,4)
-        print "</h1>\n\t\t<div class=\"content\" id=\"sm12\">",
-        print "\n\t\t\t<div class=\"menu\" id=\"sm14\" style=\"border-color: #" + cat00[:3] + ";\">\n\t\t\t\t",
-        glotInf(code,lang,1)
-        print "\n\t\t\t</div>\n\t\t</div>"
-        print "\n\t</div>",
-    else:
-      glotCrumbs(code,stage,lang)
-      print "\n\t\t<h1 id=\"sm11\">#" + cattrim + ": ",
-      glot(code,lang,stage)
-      print "</h1>\n\t\t<div class=\"content\" id=\"sm12\">",
-      if stage > 0:
-        sglot("sadd",lang,cattrim)
-      if stage > 3:
-        sglot("smet",lang,cat00)
-      else:
-        glot("smul",lang,stage)
-        print '<a href="' +MYNAME + "?lang=" + lang + "&cowc=" + cat00 + "\"> #" + cat00 + ": ",
-        glot(cat00,lang,stage)
-        sglot("scod",lang,cat00)
-      glot("shed",lang,stage)
-      print "\n\t\t\t<div class=\"menu\" id=\"sm14\" style=\"border-color: #" + cat00[:3] + ";\">\n\t\t\t\t",
-      if stage < 4:
-        glot("snot",lang)
-      glotInf(code,lang,0)
-      if stage < 4:
-        print "\n<p class=\"clearit\"> </p>"
-        showSubs(code,lang,stage)
-      print "\n\t\t\t</div>\n\t\t</div>"
-    print "\n\t</div>",
-  print "\n\t<a href=\"progress.htm\">View Progress Report</a>"
+    if stage > 0:
+      var['cattrim'] = cat00
+    if stage < 4:
+      for i in range(4 - stage): cat00 += '0'
+      var['catmul'] = cat00
+      var['muldesc'] = glot(cat00,sqlconn,lang,4) # the multiplicity topic desc will be stage 4.
+    var['catname'] = glot(code,sqlconn,lang,stage)
+    if code[0] == 'y': # Explanation key
+      if code[3] == 'x': # explain subcats
+        var['expl'] = True
+        var['catname'] = glot("sxpl",sqlconn,lang)
+        stage = 3 # the subcat explanations are stage 4, so we need to consider yxxx to be stage 3.
+      else: # explain a single subcat type
+        var['cattrim'] = '*' + code[3]
+        var['stage'] = 4 # make sure we don't show subcats
+    var['catinf'] = os.path.join("cats",lang,code)
+    var['crumbs'] = glotCrumbs(sqlconn,code,stage,lang)
+    if len(var['crumbs']) > 1: var['crumbdiv'] = True
+    var['border'] = cat00[:3]
+    if stage < 4:
+      var['subcats'] = glotSubs(sqlconn,code,stage,lang)
+  var['progress'] = glot("spro",sqlconn,lang)
+#  print "<!-- Code: " + var['code'] + " stage " + str(var['stage']) + " language: " + var['lang'] + " -->"
+
+  """
   showFoot(lang)
-  test = "SELECT ctext FROM category WHERE ccode = 'ee37';"
+  test = "SELECT ctext FROM category WHERE ccode = 'd003';"
   result = sqlconn.execute(test)
   for row in result:
     print row['ctext']
+# """
+  print template.render({'config': config, 'var': var}).encode("utf-8")
   sqlconn.close()
   exit(0)
 
--- tmpvalues.py
+++ tmpvalues.py
@@ -2,69 +2,38 @@
 
 def populate():
   dict = {}
-  dict["0xxx"] = 'Multiplicity, Collections, Search Engines, etc.'
-  dict["00xx"] = 'Portals and Aggregators'
-  dict["000x"] = 'Search Engines and Directories'
-  dict["0000"] = 'Metasearch Search Engines'
-  dict['1xxx'] = 'General Knowledge and Systems'
-  dict['2xxx'] = 'Psychology and Philosophy'
-  dict['3xxx'] = 'Communications'
-  dict['4xxx'] = 'Social Sciences'
-  dict['5xxx'] = 'Hard Sciences'
-  dict['6xxx'] = 'Medicinal Sciences, Recreation'
-  dict['7xxx'] = 'Agriculture, Horticulture, etc.'
-  dict['8xxx'] = 'Geography'
-  dict['9xxx'] = 'Technology'
-  dict['axxx'] = 'History'
-  dict['bxxx'] = 'The Arts'
-  dict['cxxx'] = 'Religion and Faith'
-  dict['dxxx'] = 'Law, Government, etc.'
-  dict['exxx'] = 'Biography, Personal sites/works, Names &amp; Insignia, Genealogy'
-  dict['fxxx'] = 'Fiction'
-  dict['03xx'] = 'Periodicals'
-  dict['04xx'] = 'Organizations and Associations'
-  dict['05xx'] = 'Bibliographies'
-  dict['0axx'] = 'Archives'
-  dict['0bxx'] = 'Collections, Collecting'
-  dict['0cxx'] = 'News'
-  dict['0fxx'] = 'Print-specific Items'
-  dict['10xx'] = 'Library and Information Science'
-  dict['11xx'] = 'Encyclopedias, Factbooks'
-  dict['12xx'] = 'Systems'
-  dict['14xx'] = 'Quotations'
-  dict['19xx'] = 'Data Processing, Computer Science, Programming, and Programs'
-  dict['1axx'] = 'Museum Science'
-  dict['sadd'] = '<p class="note" id="sm01">If you are searching for this topic, add <strong>cowc:#%s</strong> to your search criteria.</p>'
-  dict['smet'] = '<p class="note" id="sm13">The snippet for this category is: <strong>&lt;meta name="cowc" content="#%s" /&gt;</strong></p>'
-  dict['shed'] = "<p class=\"note\" id=\"sm02\">You can add this snippet to your HTML's head section to enable search engines that use CCOW to categorize your site.</p>"
-  dict["smul"] = '<p class="sugg" id="sm03">If the site/work covers multiple categories, its category might be '
-  dict["scod"] = '</a>, and the code to designate it as such would be <strong>&lt;meta name="cowc" content="#%s" /&gt;</strong>.</p>'
-  dict["snot"] = '<p class="drill" id="sm15">If this is not correct, choose a more specific category from the list below:</p>'
-  dict["soop"] = "Top (start over)"
-  dict["slan"] = "Change Language"
-  dict["ssou"] = "\n\t<p class=\"source\" id=\"sm12\">You can obtain the source for this program at http://nowhere.yet</p>"
-  dict["seng"] = "<p class=\"i18nerr\">\"%s\" could not be opened! Falling back to English. If you're seeing this in English, something is Impossible Bad Wrong.</p>"
-  dict["snia"] = "<p class=\"wip\" id=\"gI1\">The information file \"%s\" could not be found/opened. Sorry.</p>"
-  dict["sxpl"] = "Explanation of the Categories"
-  dict['scc1'] = '\n\t<div class="cc" id="sm11">This helper application is released under a <a href="http://creativecommons.org/licenses/by-sa/3.0/">Creative Commons Attribution-Share Alike</a> license.'
-  dict['scc2'] = '\n\t<p>The contents of the translation and description files are released under a <a href="http://creativecommons.org/licenses/by-nd/3.0/">CC Attribution-No Derivative Works</a> license.</p> (C) copyright 2009-2012.</div>'
-  dict["sms1"] = "One string (or more) is missing. Please contact the system administrator!"
-  dict["xxxx"] = "Anything/Everything (root level)"
-  dict["yxx0"] = "Multiplicity, the topic per se (Hol)"
-  dict["yxx1"] = "Generaltiy, core principles of the topic (Ras)"
-  dict["yxx2"] = "Mentality, Psychology, Philosophy of the topic (Dua)"
-  dict["yxx3"] = "Communicability, Education, Transmission, Transportation (Chi)"
-  dict["yxx4"] = "Sociality, Social aspects, Social impact (Ter)"
-  dict["yxx5"] = "Provability, Empiricism, Objectivity, Realism, The recent past (Fum)"
-  dict["yxx6"] = "Vitality, Life, Animals, Growth, Health, Medicine, Recreation, Leisure, The present (Sek)"
-  dict["yxx7"] = "Sustainability, Provision, Agriculture, Plants, Horticulture, Maturity (Zab/Zabat)"
-  dict["yxx8"] = "Spatiality, Community, Outer space, Locality (Med)"
-  dict["yxx9"] = "Technology, Technical aspects (Neu)"
-  dict["yxxa"] = "Memory, History, The distant past, Traditions (Uay)"
-  dict["yxxb"] = "Artistry, The arts, Vision, The seen (Arz)"
-  dict["yxxc"] = "Religion, Faith, The unseen, Rituality, Algorithm, Process, Procedure, Standards (Pax)"
-  dict["yxxd"] = "Legality, The law, Government, Policy, Political science, Rules (Ord)"
-  dict["yxxe"] = "Specificity, Individuality, Biography, Personal sites, Examples of the topic (Iyu)"
-  dict["yxxf"] = "Marginality (Controversy, Conjecture, Unaccepted topics &amp;c.), The future, Abnormalities (Ech)"
-
+  dict["en"] = {}
+  dict["en"]['sadd'] = '<p class="note" id="sm01">If you are searching for this topic, add <strong>cowc:#%s</strong> to your search criteria.</p>'
+  dict["en"]['smet'] = '<p class="note" id="sm13">The snippet for this category is: <strong>&lt;meta name="cowc" content="#%s" /&gt;</strong></p>'
+  dict["en"]['shed'] = "<p class=\"note\" id=\"sm02\">You can add this snippet to your HTML's head section to enable search engines that use CCOW to categorize your site.</p>"
+  dict["en"]["smul"] = '<p class="sugg" id="sm03">If the site/work covers multiple categories, its category might be '
+  dict["en"]["scod"] = '</a>, and the code to designate it as such would be <strong>&lt;meta name="cowc" content="#%s" /&gt;</strong>.</p>'
+  dict["en"]["snot"] = '<p class="drill" id="sm15">If this is not correct, choose a more specific category from the list below:</p>'
+  dict["en"]["soop"] = "Top (start over)"
+  dict["en"]["slan"] = "Change Language"
+  dict["en"]["ssou"] = "\n\t<p class=\"source\" id=\"sm12\">You can obtain the source for this program at http://nowhere.yet</p>"
+  dict["en"]["seng"] = "<p class=\"i18nerr\">\"%s\" could not be opened! Falling back to English. If you're seeing this in English, something is Impossible Bad Wrong.</p>"
+  dict["en"]["snia"] = "<p class=\"wip\" id=\"gI1\">The information file \"%s\" could not be found/opened. Sorry.</p>"
+  dict["en"]["sxpl"] = "Explanation of the Categories"
+  dict["en"]['scc1'] = '\n\t<div class="cc" id="sm11">This helper application is released under a <a href="http://creativecommons.org/licenses/by-sa/3.0/">Creative Commons Attribution-Share Alike</a> license.'
+  dict["en"]['scc2'] = '\n\t<p>The contents of the translation and description files are released under a <a href="http://creativecommons.org/licenses/by-nd/3.0/">CC Attribution-No Derivative Works</a> license.</p> (C) copyright 2009-2012.</div>'
+  dict["en"]["sms1"] = "One string (or more) is missing. Please contact the system administrator!"
+  dict["en"]["xxxx"] = "Anything/Everything (root level)"
+  dict["en"]["yxx0"] = "Multiplicity, the topic per se (Hol)"
+  dict["en"]["yxx1"] = "Generaltiy, core principles of the topic (Ras)"
+  dict["en"]["yxx2"] = "Mentality, Psychology, Philosophy of the topic (Dua)"
+  dict["en"]["yxx3"] = "Communicability, Education, Transmission, Transportation (Chi)"
+  dict["en"]["yxx4"] = "Sociality, Social aspects, Social impact (Ter)"
+  dict["en"]["yxx5"] = "Provability, Empiricism, Objectivity, Realism, The recent past (Fum)"
+  dict["en"]["yxx6"] = "Vitality, Life, Animals, Growth, Health, Medicine, Recreation, Leisure, The present (Sek)"
+  dict["en"]["yxx7"] = "Sustainability, Provision, Agriculture, Plants, Horticulture, Maturity (Zab/Zabat)"
+  dict["en"]["yxx8"] = "Spatiality, Community, Outer space, Locality (Med)"
+  dict["en"]["yxx9"] = "Technology, Technical aspects (Neu)"
+  dict["en"]["yxxa"] = "Memory, History, The distant past, Traditions (Uay)"
+  dict["en"]["yxxb"] = "Artistry, The arts, Vision, The seen (Arz)"
+  dict["en"]["yxxc"] = "Religion, Faith, The unseen, Rituality, Algorithm, Process, Procedure, Standards (Pax)"
+  dict["en"]["yxxd"] = "Legality, The law, Government, Policy, Political science, Rules (Ord)"
+  dict["en"]["yxxe"] = "Specificity, Individuality, Biography, Personal sites, Examples of the topic (Iyu)"
+  dict["en"]["yxxf"] = "Marginality (Controversy, Conjecture, Unaccepted topics &c.), The future, Abnormalities (Ech)"
+  dict["en"]["spro"] = "View Progress Report"
   return dict
